38a2d92f294c1097b1292e83654c0da02b352b00
Piotr Kramek <peterkramek@gmail.com>
2020-12-04 18:13:30 +0100

    Speedup by a factor of 2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from collections import namedtuple
-from copy import copy
-from pprint import pprint
+from time import time
 from typing import Tuple, List
 
 import numpy as np
 import pandas as pd
 
 Candidate = namedtuple('Candidate', ['products', 'tidlist'])
+
 
 def read_and_convert_data(file_name: str):
     df = pd.read_csv(file_name, names=['transactions'])
@@ -20,7 +20,6 @@ def get_tidlists(data_set) -> dict:
     tidlists = dict()
 
     for index, row in dataset.iterrows():
-        row_type = type(row)
         for product in row['transactions']:
             tidlists.setdefault(product, []).append(index)
 
@@ -44,34 +43,36 @@ def eclat(dataset, min_support: int):
     return frequent_itemsets
 
 
-def get_frequent_L1(tidlists: dict, min_support: int) -> Tuple[List[int], List[np.ndarray]]:
+def get_frequent_L1(tidlists: dict, min_support: int) -> Tuple[List[np.ndarray], List[np.ndarray]]:
     frequent_l1 = []
     frequent_l1_tidlists = []
 
     sorted_keys = sorted(tidlists.keys())
     for key in sorted_keys:
         if len(tidlists[key]) > min_support:
-            frequent_l1.append(key)
+            frequent_l1.append(np.array(key))
             frequent_l1_tidlists.append(np.array(tidlists[key]))
 
     return frequent_l1, frequent_l1_tidlists
 
 
-def get_frequent_L2(frequent_l1: List[int], frequent_l1_tidlists: List[np.ndarray], min_support: int):
+def get_frequent_L2(frequent_l1: List[np.ndarray], frequent_l1_tidlists: List[np.ndarray], min_support: int):
     frequent_l2 = []
     frequent_l2_tidlists = []
 
     for i in range(len(frequent_l1) - 1):
         for j in range(i + 1, len(frequent_l1)):
-            tidlist = np.intersect1d(frequent_l1_tidlists[i], frequent_l1_tidlists[j])
+            tidlist = np.intersect1d(frequent_l1_tidlists[i], frequent_l1_tidlists[j], assume_unique=True)
             if len(tidlist) > min_support:
-                frequent_l2.append([frequent_l1[i], frequent_l1[j]])
+                first = frequent_l1[i]
+                second = frequent_l1[j]
+                frequent_l2.append(np.array([first, second]))
                 frequent_l2_tidlists.append(tidlist)
 
     return frequent_l2, frequent_l2_tidlists
 
 
-def get_frequent_Lk(frequent_lk_1: List[List[int]], frequent_lk_1_tidlists: List[np.array], min_support: int):
+def get_frequent_Lk(frequent_lk_1: List[np.ndarray], frequent_lk_1_tidlists: List[np.array], min_support: int):
     frequent_lk = []
     frequent_lk_tidlists = []
     itemset_len = len(frequent_lk_1[0])
@@ -85,10 +86,9 @@ def get_frequent_Lk(frequent_lk_1: List[List[int]], frequent_lk_1_tidlists: List
             second_last = frequent_lk_1[j][itemset_len - 1]
 
             if first_last != second_last and all(np.equal(first_except_last_item, second_except_last_item)):
-                tidlist = np.intersect1d(frequent_lk_1_tidlists[i], frequent_lk_1_tidlists[j])
+                tidlist = np.intersect1d(frequent_lk_1_tidlists[i], frequent_lk_1_tidlists[j], assume_unique=True)
                 if len(tidlist) > min_support:
-                    new_candidate_itemset = copy(frequent_lk_1[i])
-                    new_candidate_itemset.append(second_last)
+                    new_candidate_itemset = np.append(np.copy(frequent_lk_1[i]), second_last)
 
                     frequent_lk.append(new_candidate_itemset)
                     frequent_lk_tidlists.append(tidlist)
@@ -96,7 +96,6 @@ def get_frequent_Lk(frequent_lk_1: List[List[int]], frequent_lk_1_tidlists: List
                 break
 
     return frequent_lk, frequent_lk_tidlists
-
 
 
 def add_results(frequent_itemsets, frequent_candidates, frequent_candidates_tidlists):
@@ -116,9 +115,12 @@ def get_frequent_candidates(candidates: dict, min_support):
     return frequent_candidates
 
 
+time_start = time()
+
 dataset = read_and_convert_data('data/BMS1_itemset_mining.txt')
 tidlists = get_tidlists(dataset)
 min_support = 50
 
 frequet_itemsets = eclat(dataset, min_support)
-pprint(frequet_itemsets)
+exec_time = time() - time_start
+print("exec time: {}".format(exec_time))
